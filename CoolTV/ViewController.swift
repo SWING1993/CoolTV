9b53871c1e731e86a6a2de46ab05a9b1d0fd9edb
SWING1993 <songguohua10446627@gmail.com>
2020-01-13 23:32:36 +0800

    update

--- CoolTV/ViewController.swift
+++ CoolTV/ViewController.swift
@@ -9,21 +9,20 @@
 import UIKit
 import AVKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
     
     
-    @IBOutlet weak var button: UIButton!
     
-    let dataSouce = ["浙江TV", "湖南tv", "CCtv"]
 
     @IBOutlet weak var collectionView: UICollectionView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+//        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cell")
+        collectionView.delegate = self
+        collectionView.dataSource = self
      
-        UICollectionView
-    
     }
     
     
@@ -31,15 +30,22 @@ class ViewController: UIViewController {
     @IBAction func click(_ sender: Any) {
         let avPlayerViewController = AVPlayerViewController()
         avPlayerViewController.player = AVPlayer.init(url: URL.init(string: "http://111.13.111.167/otttv.bj.chinamobile.com/PLTV/88888888/224/3221226338/1.m3u8")!)
-//        avPlayerViewController.videoGravity = .resize
+        avPlayerViewController.videoGravity = .resize
         avPlayerViewController.player?.play()
         self.present(avPlayerViewController, animated: true, completion: nil)
     }
     
-    @IBAction func xx(_ sender: Any) {
     
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
     }
     
-
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        return cell
+    }
+    
 }
 
